e00f14fec12e9449b95f6d285b7c5222be16c7c2
ebkn12 <ktennis.mqekr12@gmail.com>
2017-11-23 17:05:32 +0900

    add config.generators settings

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,20 @@ Bundler.require(*Rails.groups)
 module OriAppPortal
   class Application < Rails::Application
     config.time_zone = 'Asia/Tokyo'
+    config.generators do |g|
+      # ヘルパーを自動生成しない
+      g.helper false
+      # アセットファイルを自動生成しない
+      g.assets false
+      # RSpecのテストファイル自動生成に関する設定
+      g.test_framework :rspec, fixtures: true,
+                               controller_specs: true,
+                               routing_specs: false,
+                               view_specs: false,
+                               helper_specs: false,
+                               request_specs: false
+      # テストデータをfactory_botで生成する
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end
   end
 end
