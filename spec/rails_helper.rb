6ce815a5c61dca87bd356a4ae74b6645ddafbb6d
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-07 20:26:41 +0900

    modify rubocop settings

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,7 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 
 # database_cleanerの読み込み
