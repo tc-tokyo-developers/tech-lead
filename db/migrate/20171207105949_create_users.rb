504917c4e8df8b8430b25147409688c233c6ae45
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-07 22:19:25 +0900

    modify foreign_key column name

--- db/migrate/20171207105949_create_users.rb
+++ db/migrate/20171207105949_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.references :types, null: false, foreign_key: true
+      t.references :type, null: false, foreign_key: true
       t.string     :provider, null: false
       t.string     :uid, null: false
       t.string     :github_name, null: false
