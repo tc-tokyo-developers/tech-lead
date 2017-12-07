b3ea616c400d0db489f1b3df4731e208429db507
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-07 20:51:40 +0900

    define create_with_omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider    = auth[:provider]
+      user.uid         = auth[:uid]
+      user.github_name = auth[:info][:nickname]
+    end
+  end
 end
