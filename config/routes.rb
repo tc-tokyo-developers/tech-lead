922a5cb6de4cdc18b026669bcd0e845142443db6
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-07 21:03:37 +0900

    create authentication routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :student, path: '' do
+    get 'auth/:provider/callback' => 'sessions#callback'
+    get 'logout' => 'sessions#destroy'
+  end
 end
