8762ec506be108d36e9cf1352e345a666800e9c3
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-07 23:10:08 +0900

    implement edit accounts function when user sign up

--- app/controllers/student/sessions_controller.rb
+++ app/controllers/student/sessions_controller.rb
@@ -1,20 +1,18 @@
 class Student::SessionsController < ApplicationController
   def callback
     auth = request.env['omniauth.auth']
-    user = login_or_register_user(auth)
+    user = Student::User.find_by(provider: auth[:provider], uid: auth[:uid])
+    if user.blank?
+      user = Student::User.create_with_omniauth(auth)
+      redirect_to edit_student_accounts_path, notice: 'ユーザー情報を登録してください'
+    else
+      redirect_to root_url, notice: 'ログインしました'
+    end
     session[:user_id] = user.id
-    redirect_to root_url
   end
 
   def destroy
-    reest_session
-    redirect_to root_url
-  end
-
-  private
-
-  def login_or_register_user(auth)
-    user = User.find_by(provider: auth[:provider], uid: auth[:uid])
-    Student::User.create_with_omniauth(auth) if user.blank?
+    reset_session
+    redirect_to root_url, notice: 'ログアウトしました'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :student, path: '' do
     get 'auth/:provider/callback' => 'sessions#callback'
     get 'logout' => 'sessions#destroy'
+    resource :accounts, only: %i[show edit update]
   end
 
   root 'student/tmp#index'
