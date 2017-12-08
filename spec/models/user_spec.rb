a6e4aa397a4f6b1f186671424cddc2a395fb5537
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-08 21:59:08 +0900

    create user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) { create(:type, name: 'student') }
+
+  describe 'validation' do
+    context 'valid' do
+      it 'is valid with nickname, profile' do
+        expect(build(:user, type_id: :student)).to be_valid
+      end
+    end
+
+    context 'invalid' do
+      it 'is invalid without type_id' do
+        expect(build(:user, type_id: nil)).not_to be_valid
+      end
+
+      it 'is invalid without provider' do
+        expect(build(:user, type_id: :student, provider: nil)).not_to be_valid
+      end
+
+      it 'is invalid without uid' do
+        expect(build(:user, type_id: :student, uid: nil)).not_to be_valid
+      end
+
+      it 'is invalid without github_name' do
+        expect(build(:user, type_id: :student, github_name: nil)).not_to be_valid
+      end
+    end
+  end
 end
