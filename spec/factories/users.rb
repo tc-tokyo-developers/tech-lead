1176a590c9ea37ce08f682ac6f0b160fa57ebf37
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-08 21:25:07 +0900

    create factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
-    
+    type_id     %i[student mentor].sample
+    provider    'github'
+    uid         'foobar'
+    github_name 'github_name_sample'
+    nickname    nil
+    profile     nil
   end
 end
