7698bd92dae730587615f82d0b03486f04ae5f36
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-08 21:25:19 +0900

    create type spec

--- spec/models/type_spec.rb
+++ spec/models/type_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Type, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    context 'valid' do
+      it 'is valid with name' do
+        expect(build(:type)).to be_valid
+      end
+    end
+
+    context 'invalid' do
+      it 'is invalid without name' do
+        expect(build(:type, name: nil)).not_to be_valid
+      end
+    end
+  end
 end
