75e3ba62e352df4e1f1c9acec1f0461a6791d376
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-10 00:10:54 +0900

    create username method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   enum type_id: { student: 1, mentor: 2 }
 
   validates :type_id, :provider, :uid, :github_name, presence: true
+
+  def username
+    nickname? ? nickname : github_name
+  end
 end
