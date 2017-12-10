519b31f7e3e1d2f0bb70d362fc31de923ce9a9db
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-10 19:04:37 +0900

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :mentor do
+    get 'auth/:provider/callback' => 'sessions#callback'
+    get 'logout' => 'sessions#destroy'
+    resource :accounts, only: %i[show edit update]
+    root 'tmp#index'
+  end
+
   namespace :student, path: '' do
     get 'auth/:provider/callback' => 'sessions#callback'
     get 'logout' => 'sessions#destroy'
