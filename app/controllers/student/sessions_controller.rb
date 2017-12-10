763600817a443f7f5b10f90956210dee9da3b406
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-11 01:25:09 +0900

    delete unnecessary spec

--- app/controllers/student/sessions_controller.rb
+++ app/controllers/student/sessions_controller.rb
@@ -1,17 +1,6 @@
 class Student::SessionsController < Student::BaseController
   skip_before_action :authenticate_student
 
-  def callback
-    student = Student.find_from_auth_hash(auth_hash)
-    if student.present?
-      redirect_to root_url, notice: 'ログインしました'
-    else
-      student = Student.create_with_omniauth(auth_hash)
-      redirect_to edit_student_account_path, notice: 'ユーザー情報を登録してください'
-    end
-    session[:student_id] = student.id
-  end
-
   def destroy
     reset_session
     redirect_to root_url, notice: 'ログアウトしました'
