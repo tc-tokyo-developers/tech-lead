b7c9076425bba76149c7920128bc2cf9d5f8cb19
k-kohey <kawagutsuchi@gmail.com>
2018-01-13 16:29:44 +0900

    dbを作った

--- db/migrate/20180113070013_create_knowledges.rb
+++ db/migrate/20180113070013_create_knowledges.rb
@@ -1,7 +1,9 @@
 class CreateKnowledges < ActiveRecord::Migration[5.0]
   def change
     create_table :knowledges do |t|
-
+      t.string :title, null: false
+      t.text :content, null: false
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
