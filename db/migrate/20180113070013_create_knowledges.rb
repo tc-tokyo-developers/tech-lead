90d34a686e4558e8bf2541d593ca56ee0a457dbe
k-kohey <kawagutsuchi@gmail.com>
2018-01-13 21:38:30 +0900

    conflictを解消

--- db/migrate/20180113070013_create_knowledges.rb
+++ db/migrate/20180113070013_create_knowledges.rb
@@ -1,26 +1,8 @@
 class CreateKnowledges < ActiveRecord::Migration[5.0]
   def change
     create_table :knowledges do |t|
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
       t.string :title, null: false
       t.text :content, null: false
       t.references :user, foreign_key: true
-=======
-
->>>>>>> 893e1df... knowledge model を生成
-=======
-      t.string :title, null: false
-      t.text :content, null: false
-      t.references :user, foreign_key: true
->>>>>>> b7c9076... dbを作った
-=======
-      t.string :title, null: false
-      t.text :content, null: false
-      t.references :user, foreign_key: true
->>>>>>> a1d65b1ee08eed3de9cf2308d83cae731234e37e
-      t.timestamps
-    end
   end
 end
