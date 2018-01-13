71ff88761f40dddfccf1839149c3cf0ecf63ad0e
k-kohey <kawagutsuchi@gmail.com>
2018-01-13 16:09:45 +0900

    knowledge model を生成

--- db/migrate/20180113070013_create_knowledges.rb
+++ db/migrate/20180113070013_create_knowledges.rb
@@ -1,9 +1,19 @@
 class CreateKnowledges < ActiveRecord::Migration[5.0]
   def change
     create_table :knowledges do |t|
+<<<<<<< HEAD
+<<<<<<< HEAD
       t.string :title, null: false
       t.text :content, null: false
       t.references :user, foreign_key: true
+=======
+
+>>>>>>> 893e1df... knowledge model を生成
+=======
+      t.string :title, null: false
+      t.text :content, null: false
+      t.references :user, foreign_key: true
+>>>>>>> b7c9076... dbを作った
       t.timestamps
     end
   end
