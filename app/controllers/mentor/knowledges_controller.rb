7d644de724fea1ce8929a5fcc3ad051762bcb52e
k-kohey <kawagutsuchi@gmail.com>
2018-01-15 00:44:17 +0900

    sentbuttonを書き忘れてたので追加

--- app/controllers/mentor/knowledges_controller.rb
+++ app/controllers/mentor/knowledges_controller.rb
@@ -6,11 +6,9 @@ class Mentor::KnowledgesController < ApplicationController
 
   end
   def create
-
+    Knowledge.create(strong_params)
   end
-  def new
-
-  end
+  def new; end
   def edit
 
   end
@@ -19,5 +17,9 @@ class Mentor::KnowledgesController < ApplicationController
   end
   def destroy
 
+  end
+
+  def strong_params
+    params.permit(:title, :content).merge(user_id: current_mentor.id)
   end
 end
