927fd2f3625489ed3517c74b27b6c93134d08fec
k-kohey <kawagutsuchi@gmail.com>
2018-01-19 13:00:40 +0900

    student controller update

--- app/controllers/student/knowledges_controller.rb
+++ app/controllers/student/knowledges_controller.rb
@@ -1,8 +1,17 @@
 class Student::KnowledgesController < ApplicationController
   def index
-    @knowledges = Knowledge.all
+    @knowledges = Knowledge.order('id ASC')
   end
   
   def show
+    @knowledge = Knowledge.find(params[:id])
+  end
+
+  private
+
+  def knowledge_params
+    params.require(:knowledge)
+        .permit(:title, :content)
+        .merge(user_id: current_mentor.id)
   end
 end
