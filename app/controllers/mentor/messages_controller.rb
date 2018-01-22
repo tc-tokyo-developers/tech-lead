a2b843bd1031b0f48d969dcf715d8ddb1c38fc8e
水田涼介 <basket11115555@gmail.com>
2018-01-23 00:13:17 +0900

    Create chat function for mentor

--- app/controllers/mentor/messages_controller.rb
+++ app/controllers/mentor/messages_controller.rb
@@ -3,6 +3,10 @@ class Mentor::MessagesController < ApplicationController
     @messages = Message.where(chat_group_params)
   end
 
+  def create
+    Message.create()
+  end
+
   private
   def chat_group_params
     params.permit[:chat_group_id]
--- app/controllers/student/messages_controller.rb
+++ app/controllers/student/messages_controller.rb
@@ -1,4 +1,13 @@
 class Student::MessagesController < ApplicationController
   def index
+    @messages = Message.where(chat_group_params)
+  end
+
+  def create
+  end
+
+  private
+  def chat_group_params
+    params.permit[:chat_group_id]
   end
 end
