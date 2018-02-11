c3224cf30d9b8b0d5cc5200d23c7cd803d58cb73
ebkn12 <ktennis.mqekr12@gmail.com>
2018-02-11 21:11:40 +0800

    feat: create routing for chat function

--- config/routes.rb
+++ config/routes.rb
@@ -8,19 +8,21 @@
 #       mentor_account GET   /mentor/account(.:format)          mentor/accounts#show
 #                      PATCH /mentor/account(.:format)          mentor/accounts#update
 #                      PUT   /mentor/account(.:format)          mentor/accounts#update
+#   mentor_chat_groups GET   /mentor/chat_groups(.:format)      mentor/chat_groups#index
+#    mentor_chat_group GET   /mentor/chat_groups/:id(.:format)  mentor/chat_groups#show
 #          mentor_root GET   /mentor(.:format)                  mentor/tmp#index
 #       student_logout GET   /logout(.:format)                  student/sessions#destroy
 # edit_student_account GET   /account/edit(.:format)            student/accounts#edit
 #      student_account GET   /account(.:format)                 student/accounts#show
 #                      PATCH /account(.:format)                 student/accounts#update
 #                      PUT   /account(.:format)                 student/accounts#update
-#             messages GET   /messages(.:format)                messages#index
-#                      POST  /messages(.:format)                messages#create
+#     student_messages GET   /chat(.:format)                    student/messages#index
+#         student_root GET   /                                  student/tmp#index
+#                            /cable                             #<ActionCable::Server::Base:0x00007fe90afb31c8 @mutex=#<Monitor:0x00007fe90afb31a0 @mon_owner=nil, @mon_count=0, @mon_mutex=#<Thread::Mutex:0x00007fe90afb3128>>, @pubsub=nil, @worker_pool=nil, @event_loop=nil, @remote_connections=nil>
 #                 root GET   /                                  top#index
-#                      GET   /messages(.:format)                messeges#show
 # 
 
-Rails.application.routes.draw do  
+Rails.application.routes.draw do
   get 'sessions/callback'
 
   get 'auth/:provider/callback' => 'sessions#callback'
@@ -28,19 +30,17 @@ Rails.application.routes.draw do
   namespace :mentor do
     get 'logout' => 'sessions#destroy'
     resource :account, only: %i[show edit update]
-    resources :chat_groups, only: %i[index show] do
-      resources :messages, only: :create
-    end
+    resources :chat_groups, only: %i[index show]
     root 'tmp#index'
   end
 
   namespace :student, path: '' do
     get 'logout' => 'sessions#destroy'
     resource :account, only: %i[show edit update]
-    resources :messages, only: %i[index create]
+    resources :messages, path: 'chat', only: :index
+    root 'tmp#index'
   end
 
-  resources :messages, only: [:index, :create]
-
+  mount ActionCable.server => '/cable'
   root 'top#index'
 end
